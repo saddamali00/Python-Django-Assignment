8ffe3d7a814f1cb4c035bc09030bbd7c9039ce50
Mohammad Saddam Ali <mohdsaddamali002@gmail.com>
2024-08-07 18:09:25 +0545

    condition

---
+++ assignment.py
@@ -0,0 +1,51 @@
+# # ARTHEMETIC OPERATOR
+# a = 10
+# b = 5
+
+# c = a+b 
+# d = a-b  
+# e = a*b 
+# f = a%b  
+# g = a/b
+# h = a//b
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+# print(h)
+
+
+# # COMPARISON OPERATORS
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(a>b)
+# print(a<=b)
+# print(a>=b)
+
+# # LOGICAL OPERATORS
+# print(a < b and a > b)
+# print(a < b and b < a)
+# print(a < b or a > b)
+# print(a < b or b < a)
+
+# # ASSIGNMENT OPERATORS
+
+# a = b
+# a += b
+# a -= b
+# a += 5
+# b -= 10
+# print(a)
+# print(b)
+
+
+name = "shahzayn002@gmail.com"
+split_name = name.split("@")
+
+print(split_name)
+
+
+# ASSIGNMENT
+# WRITE 10 STRING MANIPULATION PRE DEFINED FUNCTION EXAMPLE
---
+++ condition.py
@@ -0,0 +1,10 @@
+# write a programme which take input as age and print they are old or not
+
+age = int(input("Write age: "))
+
+if age < 50:
+    print("You are not old.")
+else:
+    print("you are old.")
+
+
