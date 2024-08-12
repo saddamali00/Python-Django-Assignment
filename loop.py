72ea34da03d4be78ceb6078837badff9ed6d351e
Mohammad Saddam Ali <mohdsaddamali002@gmail.com>
2024-08-12 17:30:36 +0545

    while loop

---
+++ loop.py
@@ -0,0 +1,23 @@
+# WHILE LOOP
+
+# -------------INCREMENT---------------
+count = 0
+while count <= 10:
+    print(count)
+    count += 1
+    
+
+
+# -------------DECREMENT---------------
+count1 = 10
+while count1 >= 0:
+    print(count1)
+    count1 -= 1
+    
+    
+
+# -------------USING RANGE---------------
+i = 0
+while i in range(5):
+    print(i)
+    i+=1
