15abf7541bf9240198607e1c60625074ea1748d6
Mohammad Saddam Ali <mohdsaddamali002@gmail.com>
2024-08-14 21:42:42 +0545

    while loop practise

--- loop.py
+++ loop.py
@@ -1,4 +1,6 @@
-# WHILE LOOP
+import random
+
+# --------------------------------WHILE LOOP--------------------------------
 
 # -------------INCREMENT---------------
 count = 0
@@ -21,3 +23,30 @@ i = 0
 while i in range(5):
     print(i)
     i+=1
+    
+
+
+num = 1
+
+while num <= 50:
+    if num % 2 == 0:
+        print("even ", num)
+    else:
+        print("odd ",num)
+    num +=1
+
+
+# ======== ASSIGNMENT =========
+random_number = random.randint(1, 50)
+
+count = 1
+
+while count <= 5:
+    gues_number = int(input('Enter the gues numer: '))
+    if gues_number == random_number:
+        print("Congratulation you guess is correct 😊")
+        break
+    else:
+        print("Try Again")
+    count += 1
+    
