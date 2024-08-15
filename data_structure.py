71dde3eb38ea6bff112fd6269f34232f3d3c7bf0
Mohammad Saddam Ali <mohdsaddamali002@gmail.com>
2024-08-15 17:31:03 +0545

    data structure

---
+++ data_structure.py
@@ -0,0 +1,32 @@
+# names = ['shyam','hari','ram']
+# names.append('kathmandu')
+# names.insert(1,"zayn")
+
+# name1 = [1,2,3,4,5,6]
+# names.extend(name1)
+
+# name1.remove(4)
+# print(name1)
+# print(len(name1))
+
+
+list_data = [1,5,9,0,6,89,1203,4,23]
+
+# highest_number = max(list_data)
+
+# print(highest_number)
+
+# large_number = 1
+
+# for i in list_data:
+#     if i > large_number:
+#         large_number = i
+
+# print(large_number)
+
+sum = 0
+
+for i in list_data:
+    sum+=i
+    
+print(sum)
